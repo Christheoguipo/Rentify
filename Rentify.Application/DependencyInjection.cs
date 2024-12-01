be0b93a5848e9068a1a0163e8bfb221dab04c464
Theo <christheo.guipo@gmail.com>
2024-12-01 12:23:14 +0100

    Installed Automapper and added DI file for the Application layer.

---
+++ Rentify.Application/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Rentify.Application.MappingProfiles;
+using System.Reflection;
+
+namespace Rentify.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(UserProfile).Assembly);
+
+            return services;
+        }
+    }
+}
