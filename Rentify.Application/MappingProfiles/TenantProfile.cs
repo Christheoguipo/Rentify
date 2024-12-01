107e33e0ca81661d068ae3eca7453f44f4403ce6
Theo <christheo.guipo@gmail.com>
2024-12-01 12:22:00 +0100

    Added basic mapping to some models.

---
+++ Rentify.Application/MappingProfiles/TenantProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Rentify.Application.DTOs.Tenant;
+using Rentify.Domain.Entities;
+
+namespace Rentify.Application.MappingProfiles
+{
+    public class TenantProfile: Profile
+    {
+        public TenantProfile()
+        {
+            CreateMap<Tenant, TenantDto>().ReverseMap();
+            CreateMap<CreateTenantDto, Tenant>();
+            CreateMap<EditTenantDto, Tenant>();
+        }
+    }
+}
---
+++ Rentify.Application/MappingProfiles/UserProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Rentify.Application.DTOs;
+using Rentify.Domain.Entities;
+
+namespace Rentify.Application.MappingProfiles
+{
+    public class UserProfile: Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<User, UserDto>().ReverseMap();
+        }
+    }
+}
